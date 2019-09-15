9264bdd04f3bd02be98ab7b1d032179c6b7ae9ce
JongHyeokLee <jonghyeoklee90@gmail.com>
2019-09-15 23:07:20 +0200

    created the post db

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route GET api/posts
-// @desc Test route
-// @acess Public
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => res.send('post route'));
+// @route POST api/posts
+// @desc Create route
+// @acess Private
+
+router.post('/',
+    [auth,
+        [
+            check('text', 'Text is required')
+                .not()
+                .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array()});
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            const newPost = new Post ({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            });
+
+            const post = await newPost.save();
+            res.json(post);
+
+        } catch(err){
+            console.log("error in post.js", err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 module.exports = router;
