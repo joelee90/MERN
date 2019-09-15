780ffa582860683b9d8d60ba8a36d4edb42a59dd
JongHyeokLee <jonghyeoklee90@gmail.com>
2019-09-15 22:37:19 +0200

    Get github repos for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -306,6 +308,32 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
         res.json(profile);
     } catch(err) {
         console.log('error in profile.js education delete', err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route GET api/profile/github/:username
+// @desc  Get user repos from Github
+// @acess Public
+
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent' : 'node.js'}
+        };
+
+        request(options, (error, response, body) => {
+            if(error) console.error(error);
+            if(response.statusCode !== 200) {
+                return res.status(404).json({ msg: 'No Github profile found' });
+            }
+            res.json(JSON.parse(body));
+        });
+
+    } catch(err) {
+        console.log("err in get github", err.message);
         res.status(500).send('Server Error');
     }
 });
