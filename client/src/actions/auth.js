be942f7b75dc1599c503259cb96bff6f69f56112
JongHyeokLee <jonghyeoklee90@gmail.com>
2019-09-27 17:35:26 +0200

    profile component

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
     AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
-    LOGOUT
+    LOGOUT,
+    CLEAR_PROFILE
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -86,5 +87,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout / Clear Profile
 export const logout = () => dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
     dispatch({ type: LOGOUT });
 };
