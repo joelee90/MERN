220a020e4d421e51d391b9e1e77d43899b40703d
JongHyeokLee <jonghyeoklee90@gmail.com>
2019-10-20 00:58:42 +0200

    heroku deployment error fix

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,8 +1,8 @@
-import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
 
 const initialState = [];
 
-export default function (state = initialState, action) {
+export default function(state = [], action) {
     const { type, payload } = action;
 
     switch (type) {
